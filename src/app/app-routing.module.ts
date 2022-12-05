b429369b83af6a7395db3ca2e057d61885a5099a
mariamayman62 <mariamayman6298@gmail.com>
2022-12-05 20:53:59 +0200

    add Some Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { MainLayoutComponent } from './Components/main-layout/main-layout.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { UserRegisterComponent } from './Components/user-register/user-register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   // {path:'Main',component:MainLayoutComponent,children:[
+    {path:'',component:MainLayoutComponent,children:[
+      {path:'',redirectTo:'Home',pathMatch:'full'},// Default path 
+      {path:'Home',component: HomeComponent},
+    ]},
+     {path:'register',component: UserRegisterComponent},
+     {path:'**',component:NotFoundComponent}//wildcard path 
+    
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { SideMenuComponent } from './Components/side-menu/side-menu.component';
+import { UserRegisterComponent } from './Components/user-register/user-register.component';
+import { MainLayoutComponent } from './Components/main-layout/main-layout.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { HomeComponent } from './Components/home/home.component';
+import { HeaderComponent } from './Components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    SideMenuComponent,
+    UserRegisterComponent,
+    MainLayoutComponent,
+    NotFoundComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
